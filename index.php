ead1a36512fdb64ce1c3a4f9a3c9cafd68dde372
Mariana <marianne.lage@gmail.com>
2015-06-15 17:28:33 +0100

    Added a list of the existing tags

--- index.php
+++ index.php
@@ -14,7 +14,25 @@
 
 	else:
 		echo "<p>No content Found</p>";
-	endif;
+	endif; ?>
+
+    <article>
+        <div id="category">
+            <?php
+                $tags = get_tags();
+                echo "tags: ";
+                if ($tags) {
+                    foreach ($tags as $tag) {
+                        echo '<br><a href="' . get_tag_link ($tag->term_id) . '"> '. $tag->name . '</a>'; 
+                    }
+                }
+ 
+            ?>
+        </div>
+    </article>
+
+
+    <?php
 
 	get_footer( );
 ?>
