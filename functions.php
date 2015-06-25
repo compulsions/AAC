08ac6658bd4d6e9689ea6018d689e9c6f8e7ac31
Sofia F. Rocha <sofiaroc.pt@gmail.com>
2015-06-25 18:53:30 +0100

    Já carrega (mais) os javascripts. Corrigi a ordem dele carregar os css

--- functions.php
+++ functions.php
@@ -2,17 +2,17 @@
 
 	function learningWordPress_resources() {
 		
-		wp_enqueue_style('style', get_template_directory_uri() . "/css/style.css");
 		wp_enqueue_style('bootstrap', get_template_directory_uri() . "/css/bootstrap.min.css");
 		wp_enqueue_style('font-awesome', get_template_directory_uri() . "/css/font-awesome.min.css");
 		wp_enqueue_style('flickity-docs', get_template_directory_uri() . "/css/flickity-docs-slide.css");
 		wp_enqueue_style('lightbox', get_template_directory_uri() . "/css/lightbox.css");
+		wp_enqueue_style('style', get_template_directory_uri() . "/css/style.css");
 		
 
-		wp_enqueue_script('application/javascript', get_template_directory_uri() . "/js/vendor/modernizr.js");
-		wp_enqueue_script('application/javascript', get_template_directory_uri() . "/js/jquery.min.js");
-		wp_enqueue_script('application/javascript', get_template_directory_uri() . "/js/flickity-docs.min.js");
-		wp_enqueue_script('application/javascript', get_template_directory_uri() . "/js/randomcores.js");
+		wp_enqueue_script('modernizer', get_template_directory_uri() . "/js/vendor/modernizr.js");
+		wp_enqueue_script('jquery', get_template_directory_uri() . "/js/jquery.min.js");
+		wp_enqueue_script('flickity', get_template_directory_uri() . "/js/flickity-docs.min.js");
+		wp_enqueue_script('randomcores', get_template_directory_uri() . "/js/randomcores.js");
 		
 	}
 
