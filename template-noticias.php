475a05f4013097f72a87b314974bc887220d03b8
Sofia F. Rocha <sofiaroc.pt@gmail.com>
2015-07-01 05:23:58 +0100

    Mostra as notícias (mas ainda saem fora das caixas) em cartões

--- template-noticias.php
+++ template-noticias.php
@@ -2,6 +2,7 @@
 	Template Name: Notícias
 	*/
 
+	$numNoticias = 0;
 	$number_posts_per_page = 3;
 
 	$cat_id;
@@ -38,38 +39,58 @@
 	get_header();
 	include 'navbar.php';
 	include 'slideshow.php';
+
+	?>
+	<section>
+		<div id="noticias" class="container centrar cor">
+			<div class="tituloPag"> <?php echo get_the_title( ); ?></div>
+		</div>
+		<br>
 	
+	<?php
 	if (have_posts()) { ?>
-
-
-		<section>
-			<div id="noticias" class="container centrar cor">
-				<div class="tituloPag"> <?php get_the_title( ); ?></div>
-			</div>
-			<br>
-			<div class="container padding">
+		
+		<div class="container padding">
 
 		<?php while (have_posts()) : the_post()?>
 
+			<div id="noticia<?php echo $numNoticias; ?>" class="col-md-3 col-sm-4 col-xs-6 col-xxs-12 blockR">
+				<div class="noticia noticia-block cor">
+
 				<?php
+					// devia ter aqui um if para por uma classe se a foto for ao baixo ou ao alto
+					$numNoticias++;
+				?>
 
-					$titulo = get_post_meta($post->ID, 'sigla', true);
-			
-					$titulo = get_the_title();
-				
+				<a href="<?php echo get_permalink( ); ?>">
+					<div class="nomeNoticia">
+						<p><?php echo get_the_title( ); ?></p>
+					</div>
+					<div class="img_noticia_ver">
+						<img src="<?php echo get_template_directory_uri() ?>/img/notfound.png" alt="noticia">
+					</div>
+					<?php the_content( ); ?>
+					<a class="lermais" href="<?php echo get_permalink( ); ?>"> Ler mais </a>
+				</a>
 
-				//the_content( ); ?>
+				</div>
+			</div>
+
 
 		<?php endwhile; ?>
 
 		</div>
-		</section>
 
 		<?php
 
 	} else {
 		echo "<p>No content Found</p>";
 	}
+	?>
+
+	</section>
+
+	<?php
 
 	wp_reset_query();
 
