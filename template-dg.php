d8a052e1c672dbca3bd13a93fd71715335dc18c4
Sofia F. Rocha <sofiaroc.pt@gmail.com>
2015-07-05 05:09:09 +0100

    Pelouros já aparecem do lado esquerdo

--- template-dg.php
+++ template-dg.php
@@ -1,5 +1,5 @@
 <?php
-/* 
+/*
 Template Name: DG
 */
   get_header();
@@ -7,30 +7,66 @@ Template Name: DG
     include 'navbar.php';
     include 'slideshow.php';
 
-?>
-    
-    <section class="container">
-        <div class="col-md-3 col-sm-3 col-xs-12 caixa_branca barralado carrega">
-            <ul>
-                <?php
-                query_posts('cat=14');
-                if (have_posts()) : while (have_posts()) : the_post();
-                    if( get_the_tag_list() ){
-                        echo $posttags = get_the_tag_list('<li>','</li><li>','</li>');
-                    }
+    $cat_id;
+   $cats = get_categories( );
 
-                endwhile; endif; 
-                wp_reset_query();
-                ?>
-            </ul>
-        </div>
-        
-        <div class="col-md-8 col-sm-8 col-xs-12 caixa_branca">
-        CENAS
-        </div>
-    </section>
+   $custom_value = get_post_custom_values('categoria');
+   //echo "Custom value desta página: " . $custom_value[0] . "<br>";
+   //print_r($cats);
+
+   // ir ver a variável que a página passa
+   // comparar com os nomes das categorias
+   // só mostrar os posts dessa categoria
+
+   foreach ($cats as $cat) {
+   	//echo "ID: " . $cat->term_id . " Nome: ". $cat->name . "<br>";
+
+   	if ($cat->name == $custom_value[0]) {
+   		//echo "ENCONTREI-O!<br>";
+         //echo "ESCOLHIDO - >ID: " . $cat->cat_id . " Nome: ". $cat->name . "<br>";
+   		$cat_id = $cat->term_id;
+   	}
+   }
+
+
+   //Mostra todas as páginas dessa categoria
+   query_posts(array('cat' =>$cat_id, 'posts_per_page' => -1));
+?>
+
+<section class="container">
+
+   <div class="col-md-3 col-sm-3 col-xs-12 caixa_branca barralado carrega">
+      <ul>
+
+         <?php
+         if (have_posts()) {
+            while (have_posts()) : the_post(); ?>
+
+               <li><a href="<?php echo get_permalink(); ?>"><?php echo get_the_title(); ?></a></li>
+
+            <?php endwhile;
+         }
+
+         wp_reset_query();
+         ?>
+
+      </ul>
+   </div>
+
+   <div class="col-md-8 col-sm-8 col-xs-12 caixa_branca">
+
+      <?php
+      if (have_posts()) {
+         while (have_posts()) : the_post();
+            the_content();
+         endwhile;
+      }
+      ?>
+
+   </div>
+
+</section>
 
 <?php
-    get_footer();
+   get_footer();
 ?>
-
