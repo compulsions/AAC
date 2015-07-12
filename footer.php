6424583acd34581e7aeac2210a735bead03a68c1
Beatriz Diogo <beanes.diogo@gmail.com>
2015-07-12 02:03:37 +0100

    footer- css

--- footer.php
+++ footer.php
@@ -2,29 +2,32 @@
 		<div class="container">
 
 			<div class="row barra_preta">
-				<div class="col-md-3 col-sm-4 col-xs-6 col-xxs-6">
+				<div class="col-md-3 col-sm-4 col-xs-4 col-xxs-4 centrar footer-top">
 					<br>
 					<p><a href="https://www.facebook.com/DGAAC?fref=ts" target="_blank">Facebook</a></p>
 					<p><a href="http://original.livestream.com/tvaac" target="_blank">TVAAC</a></p>
 					<p><a href="http://www.ruc.pt/wp-content/themes/ruc/player.php#" target="_blank">RUC</a></p>
 				</div>
 
-				<div class="col-md-3 col-sm-6 col-xs-6 col-xxs-3 centrar">
+				<div class="col-md-3 col-sm-6 col-xs-4 col-xxs-8 centrar footer-top">
 					<br>
 					<p>Tel: 239 410 400</p>
 					<p>Fax: 239 410 439</p>
 					<p>geral@academica.pt</p>
 				</div>
+				
 
-				<div class="col-md-3 col-sm-6 col-xs-6 col-xxs-3 centrar">
+				<div class="col-md-3 col-sm-6 col-xs-4 col-xxs-12 centrar footer-top">
 					<address>
 					<br>
 					<p>Rua Padre António Vieira N.º 1</p>
 					<p>3000-315 Coimbra, Portugal</p>
 					</address>
 				</div>
+				<div class="col-md-0 col-sm-3 col-xs-3 col-xxs-0">
+				</div>
 
-				<div class="col-md-3 col-sm-6 col-xs-6 col-xxs-3">
+				<div class="col-md-3 col-sm-6 col-xs-6 col-xxs-12 centrar">
 
 					<div class="col-md-4 col-sm-4 col-xs-4 col-xxs-4 centrar">
 						<img align="center" class="patro pw_100" align="center" src="<?php echo get_template_directory_uri() . "/img/patrocinadores/uc.png" ?>">
