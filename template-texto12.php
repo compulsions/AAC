30832b6c1694d4d7d30084aeaecee0a42872c66a
Beatriz Diogo <beanes.diogo@gmail.com>
2015-08-29 20:39:33 +0100

    Parte do mapa, contactos.... NÃO CONSIGO VER A PAGINA DOS CONTACTOS POR CAUSA DO WORDPRESS MARAVILHOSOS, POR ISSO NÃO FAÇO A MINIMA COMO FICOU

--- template-texto12.php
+++ template-texto12.php
@@ -19,9 +19,43 @@ Template Name: Texto 12
                 $content = apply_filters('the_content', $page->post_content); 
                 echo $content;
             ?>
+            <div id="mapa1" class="mapa">
+                 <p><div id="mapa-1" style="height:150px;"></div></p>
+           </div>      
         </div>
     </section>
 
+<script>
+          function initialize() {
+
+            var lat =40.2097066;
+            var lon = -8.4211913;
+
+            var LatLng =new google.maps.LatLng(lat,lon);
+            var mapCanvas = document.getElementById('mapa-1');
+
+            var mapOptions = {
+              center: LatLng , 
+              zoom: 16,
+              mapTypeId: google.maps.MapTypeId.ROADMAP,
+              keyboardShortcuts: false,
+              streetViewControl: false,
+              mapTypeControl: false
+            };
+            var map = new google.maps.Map(mapCanvas, mapOptions);
+                   
+
+            var marker = new google.maps.Marker({
+                position: LatLng ,
+                map: map,
+                title: 'Coimbra'
+            });
+            marker.setIcon('img/aac2.png');
+            marker.setMap(map);1
+         }
+         }
+        google.maps.event.addDomListener(window, 'load', initialize);
+    </script>
 <?php
     wp_reset_query();
 
