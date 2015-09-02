73b54e016857b87cf93e816ec4de31c3a647a51e
Beatriz Diogo <beanes.diogo@gmail.com>
2015-09-02 23:21:41 +0100

    mais detalhes do bloco pdf

--- template-noticias.php
+++ template-noticias.php
@@ -63,13 +63,14 @@
    <section>
         <div id="livro" class="container centrar cor">
             <div class="row">
-                <div class="col-sm-6 col-xs-6 col-xxs-12 ">
-                  <h2 class="tituloPDF">Podes consultar aqui o livro “Educação: Uma Visão de Futuro” </h2>
+                <div class="col-sm-6 col-xs-6 col-xxs-12 blockPDF">
+                  <h2 class="tituloPDF">Podes consultar aqui o livro</h2>
+                  <h2 class="tituloPDF"> “Educação: Uma Visão de Futuro” </h2>
                 </div>
                 <div class="col-md-6 livro-edu">
                    <h2>
                     <a href="http://novo.academica.pt/wp-content/uploads/2015/09/uma-visao-de-futuro.pdf" target="_blank">
-                     Educação: Uma Visão de Futuro - pdf
+                     Educação: Uma Visão de Futuro
                   </a>
                   </h2>
                 </div>
